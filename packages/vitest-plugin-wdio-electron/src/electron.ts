e1f538c2e1eb4235768cf6fced07162cd79604f7
mato533 <35281743+mato533@users.noreply.github.com>
2024-12-02 10:11:55 +0900

    refactor: update variable name

--- packages/vitest-plugin-wdio-electron/src/electron.ts
+++ packages/vitest-plugin-wdio-electron/src/electron.ts
@@ -11,18 +11,24 @@ import {
 
 import type { VitestElectronOptions } from '../types'
 
-const initBrowser = async (_options: VitestElectronOptions) => {
-  const opts = _options[WDIO_ELECTRON_SERVICE_OPTIONS] || {}
-  const launcherConfig = _options.rootDir ? { rootDir: _options.rootDir } : {}
+const initBrowser = async (options: VitestElectronOptions) => {
+  const opts = options[WDIO_ELECTRON_SERVICE_OPTIONS] || {}
+  const launcherConfig = options.rootDir ? { rootDir: options.rootDir } : {}
 
   const capabilities = {
     browserName: 'electron',
-    [WDIO_ELECTRON_SERVICE_OPTIONS]: opts,
-    [WDIO_CHROMEDRIVER_OPTIONS]: _options[WDIO_CHROMEDRIVER_OPTIONS] || {},
-    [GOOG_CHROME_OPTIONS]: _options[GOOG_CHROME_OPTIONS] || {},
+    [WDIO_ELECTRON_SERVICE_OPTIONS]:
+      options[WDIO_ELECTRON_SERVICE_OPTIONS] || {},
+    [WDIO_CHROMEDRIVER_OPTIONS]: options[WDIO_CHROMEDRIVER_OPTIONS] || {},
+
+    [GOOG_CHROME_OPTIONS]: options[GOOG_CHROME_OPTIONS] || {},
   }
 
-  const launcher = new Launcher(opts, {}, launcherConfig)
+  const launcher = new Launcher(
+    capabilities[WDIO_ELECTRON_SERVICE_OPTIONS],
+    {},
+    launcherConfig
+  )
   const service = new ElectronWorkerService(opts)
   await launcher.onPrepare({}, [capabilities])
 
