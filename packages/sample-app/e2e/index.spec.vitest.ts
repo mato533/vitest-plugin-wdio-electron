442cf4d7e5363935b93e0ca3714f49af01b094e9
mato533 <35281743+mato533@users.noreply.github.com>
2024-12-10 17:39:23 +0900

    ci: update ci workflows (#10)
    ### Description
    update CI/CD pipeline.

--- packages/sample-app/e2e/index.spec.vitest.ts
+++ packages/sample-app/e2e/index.spec.vitest.ts
@@ -1,32 +1,18 @@
-// import { readFileSync } from 'node:fs'
-// import { join } from 'node:path'
-
-// const packageJson = JSON.parse(
-//   readFileSync(join(__dirname, '..', 'package.json'), {
-//     encoding: 'utf-8',
-//   })
-// )
-// const { name, version } = packageJson
-
 describe('vitest', () => {
   beforeEach(async ({ browser }) => {
-    // console.log(`===== beforeEach ${browser}`)
     await browser.$('#btn-reset').click()
     expect(await browser.$('#counter').getText()).toBe('0')
   })
 
-  it('should retrieve app metadata through the electron API', async ({
+  it('should retrieve app info through the electron API', async ({
     browser,
   }) => {
-    // const appName = await browser.electron.app('getName')
-    // expect(appName).toEqual(name)
-    // const appVersion = await browser.electron.app('getVersion')
-    // expect(appVersion).toEqual(version)
-    const r = await browser.electron.execute((electron) => {
-      console.log(electron)
-      return JSON.stringify(electron, null, '  ')
-    })
-    console.log(`result: ${r}`)
+    const r = await browser.electron.execute((electron) => ({
+      name: electron.app.getName(),
+      version: electron.app.getVersion(),
+    }))
+    expect(r.name).toBe('electron-app')
+    expect(r.version).toBe('1.0.0')
     expect(browser.electron.bridgeActive).toBeTruthy()
   })
 
@@ -42,21 +28,6 @@ describe('vitest', () => {
   it('should receive the message from main process', async ({ browser }) => {
     await browser.$('#btn-on-test').click()
     expect(await browser.$('#counter').getText()).toBe('1')
-    // console.log(await browser.$('#counter').getElement())
-    // browser.$('#counter').
     await expect(browser.$('#counter')).toBeDisplayed()
-
-    // expect('browser').toBebrowser()
   })
-  // afterAll(async () => {
-  //   if (browser) {
-  //     await browser.deleteSession()
-  //   }
-  // })
 })
-
-// const { startElectron } = await import('wdio-electron-service')
-// this.electron = await startElectron({
-//   appBinaryPath:
-//     'dist/mac-arm64/electron-app.app/Contents/MacOS/electron-app',
-// })    "@testing-library/jest-dom": "^6.6.3",
